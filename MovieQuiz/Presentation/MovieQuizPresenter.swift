5c018c5080e271ca08fc13b7abe0f61ca672b61a
Alesia Matusevich <olesia.matusevich@gmail.com>
2024-11-11 13:19:15 +0100

    Спринт 07 Рефакторинг бизнес-логика перенесена из ViewController в Presentor

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -1,26 +1,134 @@
 import UIKit
 
-final class MovieQuizPresenter {
+final class MovieQuizPresenter: QuestionFactoryDelegate  {
+    
     let questionAmount: Int = 10
     var currentQuestionIndex: Int = 0
+    var correctAnswers = 0
     
-    func isLastQuestion() -> Bool {
-            currentQuestionIndex == questionAmount - 1
-        }
-        
-        func resetQuestionIndex() {
-            currentQuestionIndex = 0
-        }
-        
-        func switchToNextQuestion() {
-            currentQuestionIndex += 1
-        }
+    weak var viewController: MovieQuizViewController?
+    private let statisticService: StatisticServiceProtocol!
     
-    func convert(model: QuizQuestion) -> QuizStepViewModel {
+    var currentQuestion: QuizQuestion?
+    var questionFactory: QuestionFactoryProtocol?
+    
+    init(viewController: MovieQuizViewController) {
+        
+        self.viewController = viewController
+        
+        statisticService = StatisticService()
+        questionFactory = QuestionFactory(moviesLoader: MoviesLoader(), delegate: self)
+        
+        questionFactory?.loadData()
+        viewController.showLoadingIndicator()
+    }
+    
+    // MARK: - Public Methods
+    
+    func restartGame() {
+        resetQuestionIndex()
+        questionFactory?.requestNextQuestion()
+    }
+    
+    func answerGived(answer: Bool) {
+        guard let currentQuestion else { return }
+        self.proceedWithAnswer(isCorrect: answer == currentQuestion.correctAnswer)
+    }
+    
+    // MARK: - Private Methods
+    
+    private func isLastQuestion() -> Bool {
+        currentQuestionIndex == questionAmount - 1
+    }
+    
+    private func resetQuestionIndex() {
+        currentQuestionIndex = 0
+        correctAnswers = 0
+    }
+    
+    private func switchToNextQuestion() {
+        currentQuestionIndex += 1
+    }
+    
+    private func didCorrectAnswer(){
+        correctAnswers += 1
+    }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
         .init(
             image: UIImage(data: model.image) ?? UIImage(),
             question: model.text,
             questionNumber: "\(currentQuestionIndex + 1)/\(questionAmount)"
         )
+    }
+    
+    private func proceedToNextQuestionOrResults() {
+        if self.isLastQuestion() {
+            
+            statisticService?.store(correct: correctAnswers, total: self.questionAmount)
+            
+            let resultText = makeResultsMessage()
+            
+            let viewModel = AlertModel(
+                title: "Этот раунд окончен!",
+                message: resultText,
+                buttonText: "Сыграть ещё раз")
+            viewController?.showAlert(result: viewModel)
+        } else {
+            self.switchToNextQuestion()
+            questionFactory?.requestNextQuestion()
+        }
+    }
+    
+    private func makeResultsMessage() -> String {
+        
+        let bestGame = statisticService.bestGame
+        
+        let totalPlaysCountLine = "Количество сыгранных квизов: \(String(statisticService?.gamesCount ?? 0))"
+        let currentGameResultLine = "Ваш результат: \(correctAnswers)\\\(questionAmount)"
+        let bestGameInfoLine = "Рекорд: \(bestGame.correct)\\\(bestGame.total)"
+        + " (\(bestGame.date.dateTimeString))"
+        let averageAccuracyLine = "Средняя точность: \(String(format: "%.2f", statisticService?.totalAccuracy ?? 0))%"
+        
+        let resultMessage = [
+            currentGameResultLine, totalPlaysCountLine, bestGameInfoLine, averageAccuracyLine
+        ].joined(separator: "\n")
+        
+        return resultMessage
+    }
+    
+    private func proceedWithAnswer(isCorrect: Bool) {
+        if isCorrect {
+            didCorrectAnswer()
+            viewController?.updateBorderColor(color: UIColor.ypGreen)
+        }else{
+            viewController?.updateBorderColor(color: UIColor.ypRed)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self else { return }
+            self.proceedToNextQuestionOrResults()
+        }
+        viewController?.changeStateButton(isEnabled: false)
+    }
+    
+    // MARK: - QuestionFactoryDelegate
+    
+    func didLoadDataFromServer() {
+        viewController?.hideLoadingIndicator()
+        questionFactory?.requestNextQuestion()
+    }
+    
+    func didFailToLoadData(with error: Error) {
+        let message = error.localizedDescription
+        viewController?.showNetworkError(message: message)
+    }
+    
+    func didReceiveNextQuestion(question: QuizQuestion?) {
+        guard let question else { return }
+        currentQuestion = question
+        let viewModel = convert(model: question)
+        DispatchQueue.main.async { [weak self] in
+            self?.viewController?.show(quiz: viewModel)
+        }
     }
 }
